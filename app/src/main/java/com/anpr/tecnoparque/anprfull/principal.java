7845628174dd8b4fdcb155c493d8f91c72f8d30f
jmesquivel19 <j.esquivel_12@hotmail.com>
2015-07-21 17:52:46 -0500

    Diseño del formulario principal modificado, sirve cámara, micrófono, y el manifest tiene todos los permisos(A falta de mejorarlo)

--- app/src/main/java/com/anpr/tecnoparque/anprfull/principal.java
+++ app/src/main/java/com/anpr/tecnoparque/anprfull/principal.java
@@ -1,14 +1,14 @@
 package com.anpr.tecnoparque.anprfull;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.provider.MediaStore;
+import android.speech.RecognizerIntent;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.ActionBar;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
-import android.content.Context;
-import android.os.Build;
 import android.os.Bundle;
-import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -16,12 +16,22 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.support.v4.widget.DrawerLayout;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+
+import java.util.ArrayList;
 
 
 public class principal extends ActionBarActivity
         implements NavigationDrawerFragment.NavigationDrawerCallbacks {
-
+    private static final int VOICE_RECOGNITION_REQUEST_CODE = 1;
+    private EditText Txtvoz;
+    private Button botoncamara;
+    private Button botonMicrofono;
+    private Button botonConsulta;
+    private Button botonEntrada;
+    private Button botonSalida;
     /**
      * Fragment managing the behaviors, interactions and presentation of the navigation drawer.
      */
@@ -33,10 +43,12 @@ public class principal extends ActionBarActivity
     private CharSequence mTitle;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(final Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_principal);
-
+        botoncamara = (Button)findViewById(R.id.btnCamara);
+        botonMicrofono = (Button)findViewById(R.id.btnMicrofono);
         mNavigationDrawerFragment = (NavigationDrawerFragment)
                 getSupportFragmentManager().findFragmentById(R.id.navigation_drawer);
         mTitle = getTitle();
@@ -45,6 +57,21 @@ public class principal extends ActionBarActivity
         mNavigationDrawerFragment.setUp(
                 R.id.navigation_drawer,
                 (DrawerLayout) findViewById(R.id.drawer_layout));
+
+        botoncamara.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT,savedInstanceState);
+                startActivityForResult(cameraIntent, 0);
+            }
+        });
+        botonMicrofono.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                startVoiceRecognitionActivity();
+            }
+        });
     }
 
     @Override
@@ -55,7 +82,52 @@ public class principal extends ActionBarActivity
                 .replace(R.id.container, PlaceholderFragment.newInstance(position + 1))
                 .commit();
     }
+    private void startVoiceRecognitionActivity() {
+        // Definición del intent para realizar en análisis del mensaje
+        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        // Indicamos el modelo de lenguaje para el intent
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE,
+                RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        // Definimos el mensaje que aparecerá
+        intent.putExtra(RecognizerIntent.EXTRA_PROMPT,"Habla Ahora ...");
 
+        // Lanzamos la actividad esperando resultados
+        startActivityForResult(intent, VOICE_RECOGNITION_REQUEST_CODE);
+    }
+    //Recogemos los resultados del reconocimiento de voz
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        String[] pla;
+        String placa="";
+        Txtvoz = (EditText) findViewById(R.id.txtPlaca);
+        //Si el reconocimiento a sido bueno
+        if(requestCode == VOICE_RECOGNITION_REQUEST_CODE && resultCode == RESULT_OK){
+            //El intent nos envia un ArrayList aunque en este caso solo
+            //utilizaremos la pos.0
+            ArrayList<String> matches = data.getStringArrayListExtra
+                    (RecognizerIntent.EXTRA_RESULTS);
+            //Separo el texto en palabras.
+
+            pla = matches.get(0).toString().split(" ");
+
+            for (int i = 0; i < pla.length; i++) {
+                bucle2:
+                for(int j=0; j<pla[i].length();j++){
+                    if(Character.isDigit(pla[i].toString().charAt(j))){
+                        placa += pla[i].toString();
+                        break bucle2;
+                    }
+                }
+
+                if(Character.isLetter(pla[i].toString().charAt(0))){
+                    placa += pla[i].toString().charAt(0);
+                }
+
+            }
+            Txtvoz.setText(placa.toUpperCase());
+            //Txtvoz.setText(matches.get(0).toString());
+            //Txtvoz.setText(placa.toUpperCase());
+        }
+    }
     public void onSectionAttached(int number) {
         Activity activity= null;
         switch (number) {
--- app/src/main/java/com/anpr/tecnoparque/anprfull/registro_vehicular.java
+++ app/src/main/java/com/anpr/tecnoparque/anprfull/registro_vehicular.java
@@ -4,9 +4,12 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.Window;
+import android.view.WindowManager;
 
 
 public class registro_vehicular extends ActionBarActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
